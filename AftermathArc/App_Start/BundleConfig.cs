2e803d1511628a80ec31bb3b73bff9fbfb962b99
Asim Babar <asim.babar1037@gmail.com>
2018-06-22 19:23:17 -0400

    Added images to the home page podcasts and changed the details page

--- AftermathArc/App_Start/BundleConfig.cs
+++ AftermathArc/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace AftermathArc
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                         "~/Scripts/Scripts.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css"));
         }
